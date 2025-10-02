03d5f4112ddfbe86ecd8fadde8406756b14e0305
MamounKolovos <mamoun.kolovos@gmail.com>
2025-10-02 13:29:30 -0400

    refactor(utils): move general utility functions to __init__.py

--- src/where_it_went/utils/__init__.py
+++ src/where_it_went/utils/__init__.py
@@ -1,3 +1,109 @@
-from .general import pipe, try_map
+from functools import reduce
+from typing import Any, Callable, overload
 
-__all__ = ["pipe", "try_map"]
+
+@overload
+def pipe[a, b](
+  value: a,
+  a: Callable[[a], b],
+) -> b: ...
+
+
+@overload
+def pipe[a, b, c](
+  value: a,
+  a: Callable[[a], b],
+  b: Callable[[b], c],
+) -> c: ...
+
+
+@overload
+def pipe[a, b, c, d](
+  value: a,
+  a: Callable[[a], b],
+  b: Callable[[b], c],
+  c: Callable[[c], d],
+) -> d: ...
+
+
+@overload
+def pipe[a, b, c, d, e](
+  value: a,
+  a: Callable[[a], b],
+  b: Callable[[b], c],
+  c: Callable[[c], d],
+  d: Callable[[d], e],
+) -> e: ...
+
+
+@overload
+def pipe[a, b, c, d, e, f](
+  value: a,
+  a: Callable[[a], b],
+  b: Callable[[b], c],
+  c: Callable[[c], d],
+  d: Callable[[d], e],
+  e: Callable[[e], f],
+) -> f: ...
+
+
+@overload
+def pipe[a, b, c, d, e, f, g](
+  value: a,
+  a: Callable[[a], b],
+  b: Callable[[b], c],
+  c: Callable[[c], d],
+  d: Callable[[d], e],
+  e: Callable[[e], f],
+  f: Callable[[f], g],
+) -> g: ...
+
+
+@overload
+def pipe[a, b, c, d, e, f, g, h](
+  value: a,
+  a: Callable[[a], b],
+  b: Callable[[b], c],
+  c: Callable[[c], d],
+  d: Callable[[d], e],
+  e: Callable[[e], f],
+  f: Callable[[f], g],
+  g: Callable[[g], h],
+) -> h: ...
+
+
+@overload
+def pipe[a, b, c, d, e, f, g, h, i](
+  value: a,
+  a: Callable[[a], b],
+  b: Callable[[b], c],
+  c: Callable[[c], d],
+  d: Callable[[d], e],
+  e: Callable[[e], f],
+  f: Callable[[f], g],
+  g: Callable[[g], h],
+  h: Callable[[h], i],
+) -> i: ...
+
+
+@overload
+def pipe[a, b, c, d, e, f, g, h, i, j](
+  value: a,
+  a: Callable[[a], b],
+  b: Callable[[b], c],
+  c: Callable[[c], d],
+  d: Callable[[d], e],
+  e: Callable[[e], f],
+  f: Callable[[f], g],
+  g: Callable[[g], h],
+  h: Callable[[h], i],
+  i: Callable[[i], j],
+) -> j: ...
+
+
+def pipe(value: Any, *funcs: Any):  # pyright: ignore[reportInconsistentOverload]
+  return reduce(
+    lambda val, func: func(val),
+    funcs,
+    value,
+  )
